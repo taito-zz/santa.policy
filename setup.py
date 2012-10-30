b0bf355b1c2c27309c5f6758120921b18ae9a802
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-10-30 22:57:58 +0200

    Release 0.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='santa.policy',
-    version='0.9',
+    version='0.10',
     description="Turns Plone site into Santa site.",
     long_description=open("README.rst").read(),
     # Get more strings from
